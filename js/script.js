f3b82c1bf5d94f7c605b4d21b3f5fc31766f1a76
amandaalvesss <amandaalcantara98@hotmail.com>
2024-06-07 10:46:42 -0300

    ajustes no layout  responsivo

--- js/script.js
+++ js/script.js
@@ -5,8 +5,11 @@ const proximoBtn = document.getElementById('proximo');
 let fotoIndex = 0;
 
 function mostrarFotos() {
+    const isSmallScreen = window.innerWidth <= 750;
+    const fotosPorPagina = isSmallScreen ? 1 : 3;
+
     fotos.forEach((foto, index) => {
-        if (index >= fotoIndex && index < fotoIndex + 3) {
+        if (index >= fotoIndex && index < fotoIndex + fotosPorPagina) {
             foto.style.display = 'block';
         } else {
             foto.style.display = 'none';
@@ -15,22 +18,28 @@ function mostrarFotos() {
 }
 
 anteriorBtn.addEventListener('click', () => {
+    const isSmallScreen = window.innerWidth <= 750;
+    const fotosPorPagina = isSmallScreen ? 1 : 3;
+
     if (fotoIndex > 0) {
         fotoIndex--;
         mostrarFotos();
     }
-
 });
 
 proximoBtn.addEventListener('click', () => {
-    if (fotoIndex < fotos.length - 3) {
+    const isSmallScreen = window.innerWidth <= 750;
+    const fotosPorPagina = isSmallScreen ? 1 : 3;
+
+    if (fotoIndex < fotos.length - fotosPorPagina) {
         fotoIndex++;
         mostrarFotos();
     }
 });
 
-mostrarFotos(); 
+mostrarFotos();
 
+window.addEventListener('resize', mostrarFotos);
 
 $(document).ready(function() {
     $('.seta-direita').click(function() {
@@ -42,4 +51,20 @@ $(document).ready(function() {
         $('.quarto-luxo').show(); 
         $('.quarto-simples').hide(); 
     });
+});
+
+document.getElementById("menuIcon").onclick = function() {
+    var menu = document.getElementById("menuHidden");
+    if (menu.style.display === "block") {
+        menu.style.display = "none"; // Se já estiver visível, oculta
+    } else {
+        menu.style.display = "block"; // Se estiver oculto, mostra
+    }
+};
+document.addEventListener("click", function(event) {
+    var menu = document.getElementById("menuHidden");
+    var menuIcon = document.getElementById("menuIcon");
+    if (event.target !== menu && event.target !== menuIcon) {
+        menu.style.display = "none";
+    }
 });
